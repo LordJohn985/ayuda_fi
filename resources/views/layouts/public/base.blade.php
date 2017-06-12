e80770a14161354b4ce86e291317c7d7856c6bc1
alvaro <cor.alvaro.87@gmail.com>
2017-06-12 01:48:04 -0300

    fix: se corrigio un error que hacia que no funcione el ordenar listado de gauchadas cuando el usuario no esta logueado

--- resources/views/layouts/public/base.blade.php
+++ resources/views/layouts/public/base.blade.php
@@ -10,12 +10,15 @@
     <link rel="stylesheet" href="css/vendor/fontawesome/css/font-awesome.css"/>
     <link rel="stylesheet" href="css/vendor/animate.css/animate.css"/>
     <link rel="stylesheet" href="css/vendor/bootstrap/css/bootstrap.css"/>
+    <link rel="stylesheet" href="/css/vendor/datatables/datatables.min.css"/>
+    <link rel="stylesheet" href="/css/vendor/toastr/toastr.min.css"/>
 
     <!-- App styles -->
     <link rel="stylesheet" href="css/pe-icons/pe-icon-7-stroke.css"/>
     <link rel="stylesheet" href="css/pe-icons/helper.css"/>
     <link rel="stylesheet" href="css/stroke-icons/style.css"/>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/css/style-custom.css">
 
 </head>
 
@@ -34,15 +37,16 @@
 
     @include('includes.scripts')
 
+
 </div><!--/wrapper-->
 
 <!-- Vendor scripts -->
-<script src="css/vendor/pacejs/pace.min.js"></script>
+{{--<script src="css/vendor/pacejs/pace.min.js"></script>--}}
 <!--<script src="css/vendor/jquery/dist/jquery.min.js"></script> -->
 <!--<script src="css/vendor/bootstrap/js/bootstrap.min.js"></script> -->
 
 <!-- App scripts -->
-<script src="js/luna.js"></script>
-@yield('scripts')
+{{--<script src="js/luna.js"></script>
+@yield('scripts')--}}
 </body>
 </html>
