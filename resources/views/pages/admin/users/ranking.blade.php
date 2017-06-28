c9872a96f0a5e0925de1b027d8fed50b941ec0ba
LordJohn985 <juanemirmazza@gmail.com>
2017-06-28 14:48:13 -0300

    Agregado vista de ranking

---
+++ resources/views/pages/admin/users/ranking.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.admin.base')
+
+@section('content')
+
+	<div class='content table-responsive'>
+		<table id="tableExample2" class="table table-striped table-hover">
+			<thead>
+				<tr>
+					<th>Puntaje</th>
+					<th class='no-sort'>Nombre</th>
+					<th class='no-sort'>Reputacion</th>
+				</tr>	
+			</thead>
+			<tbody>
+				@foreach($users as $user)
+					<tr>
+						<td>{{$user->score}}</td>
+						<td><a href="/user/{{$user->id}}">{{$user->name,$user->last_name}}</a></td>
+						<td>{{\App\Reputation::where('necesary_score', '<=', $user->score)->orderBy('necesary_score', 'DESC')->first()->name}}</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
+
+@endsection
